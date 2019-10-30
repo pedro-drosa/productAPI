f012c0e309c96e33e9bdf751c66f1e06f853bf42
Pedro Juraci <pedro_drosa@hotmail.com>
2019-10-30 17:55:45 -0300

    connecting to the database

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
+mongoose.connect("mongodb+srv://myusername:mypassword@cluster0-mzspv.mongodb.net/productapi?retryWrites=true&w=majority",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
 app.get("/",(req, res)=>{
     return res.send({status:true});
 });
