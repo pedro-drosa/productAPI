d25f52bf19f43de980925bf6d6df9b3db52a3576
Pedro Juraci <pedro_drosa@hotmail.com>
2019-10-30 18:57:34 -0300

    Record Creation

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+app.use(express.json());
 mongoose.connect("mongodb+srv://myusername:mypassword@cluster0-mzspv.mongodb.net/productapi?retryWrites=true&w=majority",{
     useNewUrlParser: true,
     useUnifiedTopology: true
