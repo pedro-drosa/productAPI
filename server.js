23880c0c69dfccf5aecf87fb2b880318f164eab3
Pedro Juraci <pedro_drosa@hotmail.com>
2019-10-30 18:25:13 -0300

    file restructuring

--- server.js
+++ server.js
@@ -1,20 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-
 mongoose.connect("mongodb+srv://myusername:mypassword@cluster0-mzspv.mongodb.net/productapi?retryWrites=true&w=majority",{
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
-const Product = require("./src/models/Product");
-
-app.get("/",(req, res)=>{
-    Product.create({
-        title: "React Native",
-        description:"Build native apps",
-        url:"http://github.com/facebook/react-native"
-    });
-    return res.send({status:true});
-});
+app.use('/api', require('./src/routes'));
 app.listen(8000);
---
+++ src/controllers/ProductController.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const Product = require("../models/Product");
+module.exports = {
+    async index(req, res){
+        const products = await Product.find();
+        return res.json(products);
+    },
+}
